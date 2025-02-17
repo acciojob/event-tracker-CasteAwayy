a9bdc49da9c5f36d52df00141facd6fadb7d849c
Abhishek Sikarwar <mail.abhisheksikarwar@gmail.com>
2025-02-17 16:11:25 +0530

    changed

--- src/components/toolbar.js
+++ src/components/toolbar.js
@@ -1,69 +1,106 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { fetchEvents, pastEvents, upcomingEvents } from '../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { fetchEvents, pastEvents, upcomingEvents } from "../actions";
 
 const navigate = {
-    PREVIOUS: 'PREV',
-    NEXT: 'NEXT',
-    TODAY: 'TODAY',
-    DATE: 'DATE',
-}
+    PREVIOUS: "PREV",
+    NEXT: "NEXT",
+    TODAY: "TODAY",
+    DATE: "DATE",
+};
 
 class CustomToolbar extends Component {
-
     onClickAllEvents() {
         this.props.fetchEvents();
-    };
+    }
 
     onClickPastEvents() {
         this.props.pastEvents();
-    };
-    
+    }
+
     onClickUpcomingEvents() {
         this.props.upcomingEvents();
-    };
+    }
 
     render() {
-        let { localizer: { messages }, label } = this.props;
-        return(
+        let {
+            localizer: { messages },
+            label,
+        } = this.props;
+        return (
             <div className="rbc-toolbar">
                 <span className="rbc-btn-group">
-                    <button type="button" className="btn btn-control" onClick={this.navigate.bind(null, navigate.PREVIOUS)}><i className="fa fa-arrow-left"></i> Prev</button>
+                    <button
+                        type="button"
+                        className="btn btn-control"
+                        onClick={this.navigate.bind(null, navigate.PREVIOUS)}
+                    >
+                        <i className="fa fa-arrow-left"></i> Prev
+                    </button>
                 </span>
                 <span className="rbc-btn-group">
-                    <button type="button" className="btn btn-control" onClick={this.navigate.bind(null, navigate.NEXT)}>Next <i className="fa fa-arrow-right"></i></button>
+                    <button
+                        type="button"
+                        className="btn btn-control"
+                        onClick={this.navigate.bind(null, navigate.NEXT)}
+                    >
+                        Next <i className="fa fa-arrow-right"></i>
+                    </button>
                 </span>
                 <span className="rbc-toolbar-label">{label}</span>
                 <span className="rbc-btn-group">
-                    <button type="button" className="btn btn-control" onClick={(e) => this.onClickAllEvents()}>All</button>
+                    <button
+                        type="button"
+                        className="btn btn-control"
+                        onClick={(e) => this.onClickAllEvents()}
+                    >
+                        All
+                    </button>
                 </span>
                 <span className="rbc-btn-group">
-                    <button type="button" className="btn btn-past" onClick={(e) => this.onClickPastEvents()}>Past</button>
+                    <button
+                        type="button"
+                        style={{ backgroundColor: 'rgb(222, 105, 135)' }}
+                        className="btn btn-past"
+                        onClick={(e) => this.onClickPastEvents()}
+                    >
+                        Past
+                    </button>
                 </span>
                 <span className="rbc-btn-group">
-                    <button type="button" className="btn btn-upcoming" onClick={(e) => this.onClickUpcomingEvents()}>Upcoming</button>
+                    <button
+                        type="button"
+                        style={{ backgroundColor: 'rgb(140, 189, 76)' }}
+                        className="btn btn-upcoming"
+                        onClick={(e) => this.onClickUpcomingEvents()}
+                    >
+                        Upcoming
+                    </button>
                 </span>
             </div>
-        )
+        );
     }
-    navigate = action => {
-        this.props.onNavigate(action)
-    }
+    navigate = (action) => {
+        this.props.onNavigate(action);
+    };
 }
 
 function mapStateToProps(state) {
     return {
-        events: state.events
+        events: state.events,
     };
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({ 
-        fetchEvents,
-        pastEvents, 
-        upcomingEvents 
-    }, dispatch);
+    return bindActionCreators(
+        {
+            fetchEvents,
+            pastEvents,
+            upcomingEvents,
+        },
+        dispatch
+    );
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CustomToolbar);
